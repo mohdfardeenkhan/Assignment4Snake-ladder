aef3de443a266288c62c09a481742415ea9c08e2
unknown <bittufardeen@gmail.com>
2022-06-23 23:22:58 +0530

    snake

---
+++ Uc1.java
@@ -0,0 +1,16 @@
+public class Uc1 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake ladder Game");
+
+        System.out.println("Welcome to Snake and Ladder simulator");
+        int playerPosition = 0;
+        System.out.println("Starting position is " + playerPosition );
+
+        int playerPosition1 = 0;
+        int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        System.out.println("Dice roll: " + die);
+
+    }
+
+}
